4a6fd4c8b2154a875b04794ab3fef0a6d4925ad9
Carlos Andres Berdugo Arias <carlos.berdugo@halliburton.com>
2020-04-23 19:34:26 -0500

    Changes in Travis File

--- NGFibonacci/karma.conf.js
+++ NGFibonacci/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     customLaunchers: {
-      ChromeHeadlessCI: {
+      ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
